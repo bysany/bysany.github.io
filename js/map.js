c334952428ca792dc96ff724a3ecd5b114655881
bysany <31740602+bysany@users.noreply.github.com>
2017-10-26 23:36:33 +0300

    file for site

---
+++ js/map.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+
+	var longitude = 55.756747;
+	var latitude = 37.630849;
+	var canvas = "map";
+
+	
+	function randing_map(canvas, lan, lat){			
+			var myLatlng = new google.maps.LatLng(lan,lat);
+			var myOptions = {
+						zoom: 13,
+						center: myLatlng,
+						mapTypeId: google.maps.MapTypeId.ROADMAP,
+						maxZoom   : 20,
+    					disableDefaultUI: true
+					}			
+			var map = new google.maps.Map( document.getElementById(canvas), myOptions );
+			var marker = new google.maps.Marker({
+			    position : myLatlng,
+			    map      : map,
+			    icon     : "img/point.png"
+			});
+			var styles = [
+			  {
+			    featureType: "all",
+			    stylers: [
+			      { saturation: -80 }
+			    ]
+			  },{
+			    featureType: "road.arterial",
+			    elementType: "geometry",
+			    stylers: [
+			      { hue: "#00ffee" },
+			      { saturation: 80 }
+			    ]
+			  },{
+			    featureType: "poi.business",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "on" }
+			    ]
+			  }
+			];	
+			var infowindow = new google.maps.InfoWindow({
+				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Address : </span>Russia Moscow</div> <div class='map_tel'><span class='tel'>Phone : </span>+79154108003</div></div>"
+			});	
+			
+			map.setOptions({styles: styles});
+
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+			});
+	}
+	randing_map(canvas, longitude, latitude);
+
+});
